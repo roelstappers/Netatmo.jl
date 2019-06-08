4de8931c509bb94b24d437bb458cbfc0d1af1f26
Roel Stappers <roels@xvis-m3b.int.met.no>
2019-06-08 18:26:08 +0000

    gpr.jl uses GaussianProcesses.jl

--- src/gpr.jl
+++ src/gpr.jl
@@ -5,23 +5,33 @@ using Glob
 using Dates
 using Plots
 import Netatmo
-import PyPlot
+
 using LinearAlgebra
 using IterativeSolvers
+using GaussianProcesses
 
-dtg = Dates.DateTime(2018,05,10,16)
-period = Dates.Hour(2)
+dtg = Dates.DateTime(2019,05,01,00)
+period = Dates.Hour(24)
 
-timerange = datetime2unix(dtg):60*10:datetime2unix(dtg+period)
+timerange = dtg:Minute(10):(dtg+period)
 latrange  = 59.9:0.01:60  
 lonrange  = 10.7:0.01:10.8
-latrange  = 59:0.01:60
-lonrange  = 7.:0.01:8
+#latrange  = 59:0.01:60
+#lonrange  = 7.:0.01:8
+
+
 
 df = Netatmo.read(timerange, latrange=latrange, lonrange=lonrange);
-
 println("Finished reading $(size(df,1)) rows")
 
+s = groupby(df,:id)[2]
+
+range = log(60*60*10.)
+time = Float64.(s[:time_utc]) 
+pressure = (s[:pressure] .-1000.)/10
+gp = GP(time,pressure,MeanZero(),SE(range,1.0),0.00)
+plot(gp)
+gp
 
 function RBF(row1,row2) 
     lt = 1.0/(2.0*(1.0*60.0*60.0)^2) # time length scale 

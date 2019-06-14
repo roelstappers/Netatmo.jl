1d0a8dc19882e33a80df34bb92c6777ce98a1c42
Roel Stappers <roels@xvis-m3b.int.met.no>
2019-06-14 12:56:35 +0000

    dump

--- src/kernels.jl
+++ src/kernels.jl
@@ -1,19 +1,40 @@
 module Kernels 
 
-import Base.size, Base.getindex
+import Base: size, getindex
+export SE, Interpolator,Kernel
+
+abstract type Kernel{T}  <: AbstractArray{T,2} end
 
 """Squared exponential Kernel
 """
-struct SE{T} <: AbstractMatrix{T}    
+struct SE{T} <: Kernel{T}   
     x::Array{T,1}
-    y::Array{T,1}
-    alpha::T       
+    y::Array{T,1}    
+    alpha::T  
+    # sigmaf2::T     
 end 
+
+make_SE(;range) = (x,y) -> SE(x,y,range=range)
 
 SE(x::Array{Float64}, y::Array{Float64}; range) = SE(x,y, -1.0/(2*range))
 SE(x::Array{Float64}; range) = SE(x,x; range=range)
 
 Base.size(k::SE) = (size(k.x,1),size(k.y,1))
-Base.getindex(K::SE, i1::Integer,i2::Integer) = @fastmath exp(K.alpha*abs2(K.x[i1]-K.x[i2])) + (i1==i2 ? 1.0 : 0.0 )
+Base.getindex(K::SE, i::Integer, j::Integer) = exp(K.alpha*abs2(K.x[i]-K.y[j])) 
+
+#struct Interpolator{T}
+#    Kinvval::Array{T}
+#    Kerneltype::DataType  
+#end
+
+#function Interpolator(val::Array{T},K::Kernel{T}) where T
+#    Interpolator{T}(K\val, typeof(K))
+#end 
+
+
+#function (itp::Interpolator{T})(x::T) where T
+#  Kstar = Kerneltype(x, itp.K.x, 2.) 
+#  return Kstar*itp.val    
+#end 
 
 end
--- src/testkernels.jl
+++ src/testkernels.jl
@@ -2,7 +2,7 @@ include("kernels.jl")
 import .Kernels
 using IterativeSolvers, LinearAlgebra
 
-n=10000
+n=1000
 x = randn(n)
 q = randn(n)
 
@@ -10,9 +10,24 @@ K = Kernels.SE(x,range=1.)
 
 println("solving linear system")
 # @time p1 = (K+ I)\q
-@time p2, cglog = cg(K,q,log=true)
+@time p2, cglog = cg(K+I,q,log=true)
+
+x = 5*randn(100)
+y = sin.(x)
+xstar = collect(-10:0.1:10)
+
+
+K = Kernels.make_SE(range=1.)
+
+yhat = K(xstar,x) * (K(x,x)\y)
+
+using Plots
+scatter(x,sin.(x))
+plot!(xstar,yhat)
+
 
 # println(cglog)
+
 
 
 

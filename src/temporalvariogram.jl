23275a4e783b5dd155c80f0f32700a747a238571
Roel Stappers <roels@met.no>
2019-07-10 16:28:18 +0000

    update

--- src/temporalvariogram.jl
+++ src/temporalvariogram.jl
@@ -19,8 +19,21 @@ df = CSV.read(filename,types=[Int64, Int64, Int64,Int64,Int64,Float64])
 # df[:DateTime] = DateTime.(df[:Year],df[:Month],df[:Day],df[Symbol("Time(UTC)")])
 
 groupedbyStnr = groupby(df,:Stnr)
-station=groupedbyStnr[1
-plot(autocov(station[:PO],0:1:24*10))
-plot!(autocov(station[:PO][1:30*24],0:1:24*10))
+station=groupedbyStnr[1]
+plot(autocov(station[:PO],0:1:24*10),label="blindern")
+
+Cv(σ²,ν,ρ) = d  ->  σ² * 2.0^(1.0 - ν) / gamma(ν) * ( sqrt(2ν)*d/ρ)^ν * besselk(ν, sqrt(2ν)*d/ρ)
+
+plot!(Cv(132,5/2,24*3.5), 0:1:24*10,label="matern 5/2  4.5")
+plot!(Cv(132,3/2,24*3.5), 0:1:24*10,label="matern 3/2  3.5")
+plot!(Cv(132,1,24*3.5), 0:1:24*10,  label="matern 1    3.5")
+plot!(Cv(132,0.5,24*4), 0:1:24*10,  label="matern 0.5  4.0")
+plot!(Cv(132,0.6,24*4), 0:1:24*10,  label="matern 0.6  4.0")
+
+#plot!(autocov(station[:PO][365*24*1:365*24*1 + 90*24],0:1:24*10))
+#plot
+
+#plot!(autocov(station[:PO][365*24*2:365*24*2 + 90*24],0:1:24*10))
+#plot!(autocov(station[:PO][365*24*3:365*24*3 + 90*24],0:1:24*10))
 
 

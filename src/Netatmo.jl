6f83e7878e1c0e23eb980d1467fc3760ae16776f
Roel Stappers <roel.stappers@met.no>
2019-11-05 10:27:29 +0000

    add Feather to Netatmo.jl

--- src/Netatmo.jl
+++ src/Netatmo.jl
@@ -2,15 +2,17 @@ module Netatmo
 
 import DataFrames, DataStructures
 using CategoricalArrays, CSV, JSON
-using Glob, Dates, ProgressMeter
+using Glob, Dates, ProgressMeter, Statistics 
+import Feather
 
 export read
 export json2csv
-export df2df4odbimport
-export csv2csv4odbimport
+# export df2df4odbimport
+# export csv2csv4odbimport
 
-CSV_ARCHIVE="/lustre/storeB/users/roels/netatmo/"
-JSON_ARCHIVE="/lustre/storeB/project/metproduction/products/netatmo/"
+CSV_ARCHIVE    ="/lustre/storeB/users/roels/netatmo/csv"
+FEATHER_ARCHIVE="/lustre/storeB/users/roels/netatmo/feather"
+JSON_ARCHIVE   ="/lustre/storeB/project/metproduction/products/netatmo/"
 
 function __init__()
   isdir(CSV_ARCHIVE)  || error("Directory $CSV_ARCHIVE does not exist")
@@ -22,5 +24,8 @@ end
 
 include("read.jl")
 include("json2csv.jl")
+include("json2feather.jl")
+
 include("df2df4odbimport.jl")
+include("thin.jl")
 end

85d5c5b4778045ac418612c7243f9291b8e24aa1
Roel Stappers <roel.stappers@met.no>
2020-02-19 10:51:24 +0100

    Update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using CSV
 csvfile="$(Netatmo.CSV_ARCHIVE)/2019/05/01/20190501T094501Z.csv"
 filename, ext = splitext(basename(csvfile))
 df = CSV.read(csvfile )
-df2 = df2df4odbimport(df)
+df2 = Netatmo.df2df4odbimport(df)
 CSV.write("$(filename)_temp.csv",df2)   
 
 

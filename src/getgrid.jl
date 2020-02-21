0fa91770fe5470db8a0e90d99ff3aaa69d04e745
Roel Stappers <roel.stappers@met.no>
2020-02-21 17:21:15 +0100

    Clean getgrid

--- src/getgrid.jl
+++ src/getgrid.jl
@@ -1,44 +1,39 @@
 function getgrid(stepsize)
 
-"""
-     Returns lon lat coordinates based on a regular grid created in the Lambert conformal conic projection
-     `stepsize` is the gridspacing in meters 
+    """
+     Returns lon lat coordinates based on a regular grid created in 
+     the Lambert conformal conic alternative (lcca) projection
+     `stepsize` is the grid spacing in meters 
 """
 
 # For now we fix the domain should read this from config_exp.h
-    METCOOP25C = (NLAT = 960,
-    LONC = 16.763011639,
-    TSTEP = 75,
-    GSIZE = 2500.0,
-    LON0 = 15.0,
-    LATC = 63.489212956,
-    LAT0 = 63.0,
-    NLON = 900, 
-    EZONE = 11)
+    METCOOP25C = (NLAT = 960, NLON=900, LONC = 16.763011639, LATC = 63.489212956,
+                  TSTEP = 75, GSIZE = 2500.0, LON0 = 15.0, LAT0 = 63.0, EZONE = 11)
 
     dom  = METCOOP25C
-    Plonlat = Projection("+proj=longlat +R=$Rearth")
-    Plcca   = Projection("+proj=lcca    +R=$Rearth +lat_0=$(dom.LAT0) +lon_0=$(dom.LON0) ")
 
-    lcca2lonlat(xy)     = transform(Plcca, Plonlat, xy)
-    lonlat2lcca(lonlat) = transform(Plonlat, Plcca, lonlat)
+    nlon, nlat = dom.NLON, dom.NLAT
+    lon0, lat0 = dom.LON0, dom.LAT0
+    lonc, latc = dom.LONC, dom.LATC
+    gsize, ezone = dom.GSIZE, dom.EZONE
 
-    (xc, yc) = lonlat2lcca([dom.LONC, dom.LATC])
+    Plonlat = Proj4.Projection("+proj=longlat +R=$Rearth")
+    Plcca   = Proj4.Projection("+proj=lcca    +R=$Rearth +lat_0=$lat0 +lon_0=$lon0")
 
-    west  = xc - dom.GSIZE * (dom.NLON - dom.EZONE - 1) / 2
-    south = yc - dom.GSIZE * (dom.NLAT - dom.EZONE - 1) / 2
-    east  = xc + dom.GSIZE * (dom.NLON - dom.EZONE - 1) / 2
-    north = yc + dom.GSIZE * (dom.NLAT - dom.EZONE - 1) / 2
+    lcca2lonlat(xy)     = Proj4.transform(Plcca, Plonlat, xy)
+    lonlat2lcca(lonlat) = Proj4.transform(Plonlat, Plcca, lonlat)
 
-    xs = range(west,  step = stepsize, stop = east) 
+    (xc, yc) = lonlat2lcca([lonc, latc])
+
+    west  = xc - gsize * (nlon  -ezone - 1) / 2
+    south = yc - gsize * (nlat - ezone - 1) / 2
+    east  = xc + gsize * (nlon - ezone - 1) / 2
+    north = yc + gsize * (nlat - ezone - 1) / 2
+
+    xs = range(xc,  step = stepsize, stop = east) 
     ys = range(south, step = stepsize, stop = north) 
 
-    lonlats = []
-    for x in xs
-        for y in ys        
-            push!(lonlats, lcca2lonlat([x,y]))
-        end 
-    end 
+    lonlats = [lcca2lonlat([x,y]) for x in xs for y in ys] 
 
     return lonlats
 end 
